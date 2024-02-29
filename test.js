9cfc1a1ac56ba8ece59fddc89db3c28f5d17df37
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2024-03-01 07:31:39 +0800

    dynamic kernel for test

--- test.js
+++ test.js
@@ -1,27 +1,42 @@
 import { v4 as uuidv4 } from 'uuid';
 import crypto from 'crypto'
 import zmq from 'zeromq'
+import fs from 'fs'
 
 
 async function runClient() {
-	const shell_addr = "tcp://localhost:64762"
+	exec()
+}
+
+function exec() {
+	const kernel = fs.readFile('kernel.json', 'utf8', (err, data) => {
+		if (err) {
+			console.error(err)
+			return
+		}
+
+		sendToKernel(JSON.parse(data))
+	})
+}
+
+async function sendToKernel(kernel) {
+	console.log(kernel)
+
+	const shell_addr = `${kernel.transport}://${kernel.ip}:${kernel.shell_port}`
 	const sock = new zmq.Request()
 
-	const key = "538992b0-cb3b0809ca44a9964f6c04e1"
-
-	const msg_id = uuidv4()
-	const msg_type = "execute_request"
+	const key = kernel.key
 
 	const header = {
-		msg_id,
-		msg_type
+		msg_id: uuidv4(),
+		msg_type: "execute_request"
 	}
 
 	const parent_header = {}
 	const metadata = {}
 
 	const content = {
-		code: 'x = 102',
+		code: 'x = ' + new Date().getTime(),
 		silent: false,
 		store_history: true,
 		user_expressions: {},
@@ -29,10 +44,9 @@ async function runClient() {
 		stop_on_error: true 
 	}
 
-	const sign = JSON.stringify(header)
-              + JSON.stringify(parent_header)
-              + JSON.stringify(metadata)
-              + JSON.stringify(content)
+	const strArr = [header, parent_header, metadata, content].map(JSON.stringify)
+	const sign = strArr.join('')
+
 	const hmac_digest = crypto.createHmac("sha256", key).update(sign).digest("hex")
 	console.log(hmac_digest)
 
@@ -40,10 +54,7 @@ async function runClient() {
 		"",
 		"<IDS|MSG>",
 		hmac_digest,
-		JSON.stringify(header),
-		JSON.stringify(parent_header),
-		JSON.stringify(metadata),
-		JSON.stringify(content)
+		...strArr
 	]
 
 	sock.connect(shell_addr)
