8f4dffe0b07c3c5b1d80e2871d75e4acd46356f4
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2024-03-01 07:16:49 +0800

    add logging

--- test.py
+++ test.py
@@ -2,9 +2,14 @@ if __name__ == '__main__':
 	from ipykernel.kernelapp import IPKernelApp
 	import os
 	import shutil
+	import logging
+
+	logging.basicConfig(filename='test.log', encoding='utf-8', level=logging.DEBUG)
       
 	app = IPKernelApp.instance()
 	app.initialize()
+	
+	logging.info(app.abs_connection_file)
 
 	if os.path.isfile(app.abs_connection_file):
 		shutil.copyfile(app.abs_connection_file, "./kernel.json")
